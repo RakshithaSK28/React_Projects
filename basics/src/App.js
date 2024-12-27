53f3c9ad316a8453c50d7e50b86ee475e34029ef
RakshithaSK28 <rakshithask28sk@gmail.com>
2024-12-27 10:49:03 +0530

    props concept

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,8 +1,18 @@
-
+import Home from'./Home'
+import Contact from './Contact'
+import About from './About'
+import Greeting from'./Greeting'
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <div>
+    <h1>Hello from React JS</h1>
+    <Greeting name = {'Akash'} />
+    <Greeting name = {'Diya'} />
+   {/* <Home name = {'Rakshi'}  age = {24} />
+    <About />
+    <Contact />*/}
+    </div>
   );
 }
 
